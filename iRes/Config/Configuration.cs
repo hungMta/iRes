51ddb06f39dda45a1da6da946cc2a4cfb2e84821
Hưng Trần <Hng Trn>
2016-11-30 16:47:08 +0700

    fix tiep

--- iRes/Config/Configuration.cs
+++ iRes/Config/Configuration.cs
@@ -19,8 +19,8 @@ namespace Title.Config {
 
        // public string DATA_SOURCE = @"Data Source=.\SQLEXPRESS;Initial Catalog=iResDatabase;Integrated Security=True";
         public string DATA_SOURCE = @"Data Source=DESKTOP-8IKVHJR;Initial Catalog=QuanLyNhaHang;Integrated Security=True";
-        public string PROC_GET_LIST_NHANVIEN = "GetListNhanVien";
-        public string PROC_INSERT_NHANVIEN = "InsertNhanVien";
+    
+        
         public string PROC_INSERT_KHACHHANG = "ThemKhachHang";
         public string PROC_GET_LIST_KHACHHANG = "GetListKhachHang";
         public string PROC_GET_LIST_MONAN = "GetListMonAn";
